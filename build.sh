a8b14945315f13a26f8221dde3f450a01824a32f
Kleis Auke Wolthuizen <github@kleisauke.nl>
2021-01-30 16:25:44 +0100

    Update dependencies and patches

--- build.sh
+++ build.sh
@@ -69,7 +69,7 @@ fi
 
 # Common compiler flags
 export CFLAGS="-O3 -fno-rtti -fno-exceptions -mnontrapping-fptoint"
-if [ "$SIMD" = "true" ]; then export CFLAGS+=" -msimd128 -msse2"; fi
+if [ "$SIMD" = "true" ]; then export CFLAGS+=" -msimd128"; fi
 if [ "$EXPERIMENTAL_SIMD" = "true" ]; then export CFLAGS+=" -munimplemented-simd128"; fi
 if [ -n "$LTO_FLAG" ]; then export CFLAGS+=" -flto"; fi
 if [ -n "$WASM_BIGINT_FLAG" ]; then export CFLAGS+=" -DWASM_BIGINT"; fi
@@ -89,7 +89,7 @@ export MESON_CROSS="$SOURCE_DIR/build/emscripten-crossfile.meson"
 # Dependency version numbers
 # TODO(kleisauke): GIF support is currently missing, giflib abandoned autotools which makes compilation difficult
 # Wait for https://github.com/libvips/libvips/pull/1709 instead.
-VERSION_ZLIBNG=1.9.9-b1
+VERSION_ZLIBNG=2.0.0-RC2
 VERSION_FFI=3.3
 VERSION_GLIB=2.67.2
 VERSION_EXPAT=2.2.10
@@ -97,8 +97,8 @@ VERSION_EXIF=0.6.22
 VERSION_LCMS2=2.11
 VERSION_JPEG=2.0.6
 VERSION_PNG16=1.6.37
-VERSION_SPNG=0.6.1
-VERSION_WEBP=1.1.0
+VERSION_SPNG=0.6.2
+VERSION_WEBP=1.2.0
 VERSION_TIFF=4.2.0
 VERSION_VIPS=8.10.5
 
@@ -130,9 +130,13 @@ if [ "$RUNNING_IN_CONTAINER" = true ]; then
   # https://github.com/emscripten-core/emscripten/pull/12963
   patch -p1 <$SOURCE_DIR/build/patches/emscripten-12963.patch
 
-  # Need to rebuild libc, libdlmalloc and libemmalloc, since
-  # we modified it with the patches above
-  embuilder.py build libc-mt libdlmalloc-mt{,-debug} libemmalloc-mt{,-64bit} --force $LTO_FLAG
+  # Need to rebuild libembind, libc, libdlmalloc and libemmalloc,
+  # since we modified it with the patches above
+  embuilder.py build libembind libc-mt libdlmalloc-mt{,-debug} libemmalloc-mt{,-64bit} --force $LTO_FLAG
+
+  # The system headers require to be reinstalled, as some of
+  # them have also been changed with the patches above
+  embuilder.py build sysroot --force
 fi
 
 if [ -n "$EMMAKEN_CFLAGS" ]; then
@@ -145,10 +149,10 @@ echo "Compiling zlib-ng"
 echo "============================================="
 test -f "$TARGET/lib/pkgconfig/zlib.pc" || (
   mkdir $DEPS/zlib-ng
-  curl -Ls https://github.com/zlib-ng/zlib-ng/archive/$VERSION_ZLIBNG.tar.gz | tar xzC $DEPS/zlib-ng --strip-components=1
+  curl -Ls https://github.com/zlib-ng/zlib-ng/archive/v$VERSION_ZLIBNG.tar.gz | tar xzC $DEPS/zlib-ng --strip-components=1
   cd $DEPS/zlib-ng
   # SSE intrinsics needs to be checked for wasm32
-  sed -i 's/\(|\s*x86_64\)/\1 | wasm32/g' configure
+  sed -i 's/|\s*x86_64/& | wasm32/g' configure
   # Avoid CPU checks at runtime
   sed -i 's/\s-DX86_FEATURES//g' configure
   sed -i 's/\sx86.l*o//g' configure
@@ -262,10 +266,8 @@ test -f "$TARGET/lib/pkgconfig/spng.pc" || (
   cd $DEPS/spng
   # TODO(kleisauke): Discuss this patch upstream
   patch -p1 <$SOURCE_DIR/build/patches/libspng-emscripten.patch
-  # TODO(kleisauke): Remove this patch once 0.62.2 is released
-  curl -Ls https://github.com/randy408/libspng/commit/3439bf5c173421b1af4d0f75c23325e99ac67318.patch | patch -p1
   meson setup _build --prefix=$TARGET --cross-file=$MESON_CROSS --default-library=static --buildtype=release \
-    -Dstatic_zlib=true ${DISABLE_SIMD:+-Denable_opt=false}
+    -Dstatic_zlib=true ${DISABLE_SIMD:+-Denable_opt=false} ${ENABLE_SIMD:+-Dc_args="$CFLAGS -msse2"}
   emmake ninja -C _build install
 )
 
@@ -276,11 +278,13 @@ test -f "$TARGET/lib/pkgconfig/libwebp.pc" || (
   mkdir $DEPS/webp
   curl -Ls https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-$VERSION_WEBP.tar.gz | tar xzC $DEPS/webp --strip-components=1
   cd $DEPS/webp
+  # Prepend `-msimd128` to SSE flags, see: https://github.com/emscripten-core/emscripten/issues/12714
+  sed -i 's/-msse/-msimd128 &/g' configure
   # Disable threading support, we rely on libvips' thread pool
   emconfigure ./configure --host=$CHOST --prefix=$TARGET --enable-static --disable-shared --disable-dependency-tracking \
-    ${DISABLE_SIMD:+--disable-sse2} ${ENABLE_SIMD:+--enable-sse2} --disable-sse4.1 --disable-neon --disable-gl --disable-sdl \
-    --disable-png --disable-jpeg --disable-tiff --disable-gif --disable-threading --enable-libwebpmux --enable-libwebpdemux \
-    CPPFLAGS="-DWEBP_EXTERN=extern -DWEBP_DISABLE_STATS"
+    ${DISABLE_SIMD:+--disable-sse2 --disable-sse4.1} ${ENABLE_SIMD:+--enable-sse2 --enable-sse4.1} --disable-neon \
+    --disable-gl --disable-sdl --disable-png --disable-jpeg --disable-tiff --disable-gif --disable-threading \
+    --enable-libwebpmux --enable-libwebpdemux CPPFLAGS="-DWEBP_EXTERN=extern -DWEBP_DISABLE_STATS"
   emmake make install
 )
 
@@ -305,7 +309,6 @@ test -f "$TARGET/lib/pkgconfig/vips.pc" || (
   #curl -Ls https://github.com/libvips/libvips/tarball/$VERSION_VIPS | tar xzC $DEPS/vips --strip-components=1
   cd $DEPS/vips
   # Emscripten specific patches
-  patch -p1 <$SOURCE_DIR/build/patches/vips-disable-cpp.patch
   patch -p1 <$SOURCE_DIR/build/patches/vips-remove-orc.patch
   # TODO(kleisauke): Discuss these patches upstream
   patch -p1 <$SOURCE_DIR/build/patches/vips-speed-up-getpoint.patch
@@ -317,11 +320,12 @@ test -f "$TARGET/lib/pkgconfig/vips.pc" || (
   patch -p1 <$SOURCE_DIR/build/patches/vips-1492-emscripten.patch
   #patch -p1 <$SOURCE_DIR/build/patches/vips-1492-profiler.patch
   emconfigure ./autogen.sh --host=$CHOST --prefix=$TARGET --enable-static --disable-shared --disable-dependency-tracking \
-    --disable-debug --disable-introspection --disable-deprecated --disable-cpp --with-radiance --with-analyze --with-ppm \
-    --with-libexif --with-lcms --with-jpeg --with-png --with-libwebp --with-tiff --without-giflib --without-rsvg --without-gsf \
-    --without-zlib --without-fftw --without-magick --without-OpenEXR --without-nifti --without-heif --without-pdfium \
-    --without-poppler --without-openslide --without-matio --without-cfitsio --without-pangoft2 --without-imagequant
-  emmake make install
+    --disable-debug --disable-introspection --disable-deprecated --with-radiance --with-analyze --with-ppm --with-libexif \
+    --with-lcms --with-jpeg --with-png --with-libwebp --with-tiff --without-giflib --without-rsvg --without-gsf --without-zlib \
+    --without-fftw --without-magick --without-OpenEXR --without-nifti --without-heif --without-pdfium --without-poppler \
+    --without-openslide --without-matio --without-cfitsio --without-pangoft2 --without-imagequant
+  emmake make -C 'libvips' install
+  emmake make install-pkgconfigDATA
 )
 
 echo "============================================="

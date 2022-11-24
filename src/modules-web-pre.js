50d17e8286e5b7ba9cbd3f87a76a7534057fd8bb
Kleis Auke Wolthuizen <github@kleisauke.nl>
2022-11-24 17:24:03 +0100

    Use the path portion of the URL in `Module.dynamicLibraries` on web
    Since `dlopen()` in Emscripten uses `PATH.normalize()` which doesn't
    work on https and http URLs.

--- src/modules-web-pre.js
+++ src/modules-web-pre.js
@@ -1,3 +1,3 @@
 // Load vips-jxl.wasm by default on Web
 // FIXME(kleisauke): https://github.com/emscripten-core/emscripten/issues/16240#issuecomment-1308610797
-Module['dynamicLibraries'] = Module['dynamicLibraries'] || ['vips-jxl.wasm'].map(m => locateFile(m));
+Module['dynamicLibraries'] = Module['dynamicLibraries'] || ['vips-jxl.wasm'].map(m => new URL(locateFile(m)).pathname);

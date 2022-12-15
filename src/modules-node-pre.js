1acce31713a20f88a2e2cd55e35c320363521687
Kleis Auke Wolthuizen <github@kleisauke.nl>
2022-12-15 11:06:22 +0100

    Add `vips-heif.wasm` to list of loadable dynamic modules

--- src/modules-node-pre.js
+++ src/modules-node-pre.js
@@ -1,3 +1,3 @@
-// Load vips-jxl.wasm by default on Node.js
+// Load dynamic modules by default on Node.js
 // FIXME(kleisauke): https://github.com/emscripten-core/emscripten/issues/16240#issuecomment-1308610797
-Module['dynamicLibraries'] = Module['dynamicLibraries'] || ['vips-jxl.wasm'].map(m => locateFile(m));
+Module['dynamicLibraries'] = Module['dynamicLibraries'] || ['vips-jxl.wasm', 'vips-heif.wasm'].map(m => locateFile(m));

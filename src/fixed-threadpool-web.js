d5be25a705759de07b7c2a4a1ecbf0aad11f5ef3
Kleis Auke Wolthuizen <github@kleisauke.nl>
2023-03-23 14:12:51 +0100

    Enforce a fixed threadpool only on web
    Pthreads can be safely spawned on-demand in Node.js without using
    the `PTHREAD_POOL_SIZE` setting since Emscripten 3.1.29.

---
+++ src/fixed-threadpool-web.js
@@ -0,0 +1,14 @@
+mergeInto(LibraryManager.library, {
+  $VIPS__deps: ['$ENV'],
+  $VIPS__postset: 'VIPS.enforce_fixed_threadpool();',
+  $VIPS: {
+    enforce_fixed_threadpool: () => {
+      addOnPreRun(() => {
+        // Enforce a fixed thread pool by default on web
+        ENV['VIPS_MAX_THREADS'] = {{{ PTHREAD_POOL_SIZE }}};
+      });
+    }
+  }
+});
+
+DEFAULT_LIBRARY_FUNCS_TO_INCLUDE.push('$VIPS');

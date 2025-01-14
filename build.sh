85e5ae7585d5eaef192d387c379bb40f25569465
Kleis Auke Wolthuizen <github@kleisauke.nl>
2025-01-14 11:37:55 +0100

    `WASM_BIGINT` was enabled by default
    Since Emscripten 4.0.0, see commit:
    emscripten-core/emscripten@93d2f8d

--- build.sh
+++ build.sh
@@ -144,7 +144,7 @@ if [ "$PIC" = "true" ]; then export CFLAGS+=" -fPIC"; fi
 export CXXFLAGS="$CFLAGS"
 
 export LDFLAGS="$COMMON_FLAGS -L$TARGET/lib -sAUTO_JS_LIBRARIES=0 -sAUTO_NATIVE_LIBRARIES=0"
-if [ "$WASM_BIGINT" = "true" ]; then export LDFLAGS+=" -sWASM_BIGINT"; fi
+if [ "$WASM_BIGINT" = "false" ]; then export LDFLAGS+=" -sWASM_BIGINT=0"; fi
 if [ "$WASM_EXNREF" = "true" ]; then export LDFLAGS+=" -sWASM_EXNREF"; fi
 
 # Build paths

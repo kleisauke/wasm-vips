218d17c3f6c7ae1adff0d64a6d261ca5ae520e3d
Kleis Auke Wolthuizen <github@kleisauke.nl>
2026-06-21 15:22:20 +0200

    Upgrade to libvips/libvips@e51dfe5

--- build.sh
+++ build.sh
@@ -162,7 +162,7 @@ VERSION_TIFF=732665c        # https://gitlab.com/libtiff/libtiff
 VERSION_RESVG=0.47.0        # https://github.com/linebender/resvg
 VERSION_AOM=3.14.1          # https://aomedia.googlesource.com/aom
 VERSION_HEIF=1.23.0         # https://github.com/strukturag/libheif
-VERSION_VIPS=19769cb        # https://github.com/libvips/libvips
+VERSION_VIPS=e51dfe5        # https://github.com/libvips/libvips
 
 VERSION_EMSCRIPTEN="$(emcc -dumpversion)"
 

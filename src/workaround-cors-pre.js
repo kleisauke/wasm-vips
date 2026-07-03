f35a4cd0f177af3cc4aee080df95a01bbfb46908
Kleis Auke Wolthuizen <github@kleisauke.nl>
2026-07-03 11:18:12 +0200

    Prefer use of logical OR assignment

--- src/workaround-cors-pre.js
+++ src/workaround-cors-pre.js
@@ -1,6 +1,6 @@
 // https://stackoverflow.com/q/25458104
 if (Module['workaroundCors']) {
-  Module['mainScriptUrlOrBlob'] = Module['mainScriptUrlOrBlob'] ||
+  Module['mainScriptUrlOrBlob'] ||=
     URL.createObjectURL(new Blob(
       [`importScripts('${_scriptName}');`],
       {'type': 'application/javascript'}));
